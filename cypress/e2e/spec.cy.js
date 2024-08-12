ac89351d9fac5dd6fa8a055c530d50937b73e9dd
Jesse Jurman <j.r.jurman@gmail.com>
2024-08-12 02:32:47 -0400

    using 0 -> 1, more sane

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -3,13 +3,13 @@ describe('template spec', () => {
     cy.visit('../../example.html');
 
     // works on all browsers
-    cy.get('my-counter#a').should('have.attr', 'count', '5');
+    cy.get('my-counter#a').should('have.attr', 'count', '0');
 		cy.get('my-counter#a').shadow().find('button').click();
-		cy.get('my-counter#a').should('have.attr', 'count', '6');
+		cy.get('my-counter#a').should('have.attr', 'count', '1');
 
     // does not work on chromium browsers
-    cy.get('my-counter#b').should('have.attr', 'count', '5');
+    cy.get('my-counter#b').should('have.attr', 'count', '0');
 		cy.get('my-counter#b').shadow().find('button').click();
-		cy.get('my-counter#b').should('have.attr', 'count', '6');
+		cy.get('my-counter#b').should('have.attr', 'count', '1');
   })
 })
