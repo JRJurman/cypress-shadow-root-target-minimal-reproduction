c65bf9cbf0d710c708f6321c5933c56f187746e0
Jesse Jurman <j.r.jurman@gmail.com>
2024-08-12 02:14:51 -0400

    example project with failing test when using slots

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,15 @@
+describe('template spec', () => {
+  it('tests counter buttons', () => {
+    cy.visit('../../example.html');
+
+    // works on all browsers
+    cy.get('my-counter#a').should('have.attr', 'count', '5');
+		cy.get('my-counter#a').shadow().find('button').click();
+		cy.get('my-counter#a').should('have.attr', 'count', '6');
+
+    // does not work on chromium browsers
+    cy.get('my-counter#b').should('have.attr', 'count', '5');
+		cy.get('my-counter#b').shadow().find('button').click();
+		cy.get('my-counter#b').should('have.attr', 'count', '6');
+  })
+})
